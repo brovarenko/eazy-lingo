7a335be8ef6d99f00ee910d21b4989a62716ec83
Vlad <brovwg@gmail.com>
2024-07-01 16:16:28 +0200

    fix card style

--- app/components/words.tsx
+++ app/components/words.tsx
@@ -3,16 +3,19 @@
 import { useToast } from '@/components/ui/use-toast';
 
 import { useEffect, useState } from 'react';
-import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-  CarouselNext,
-  CarouselPrevious,
-} from '@/components/ui/carousel';
-import { Card, CardContent } from '@/components/ui/card';
-import { type CarouselApi } from '@/components/ui/carousel';
+
 import { Progress } from '@/components/ui/progress';
+import { cn } from '@/lib/utils';
+
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
 
 interface Word {
   id: number;
@@ -26,8 +29,9 @@ export default function Words() {
   const [userInput, setUserInput] = useState('');
   const [score, setScore] = useState(0);
   const { toast } = useToast();
-  const [api, setApi] = useState<CarouselApi>();
+
   const [progress, setProgress] = useState(13);
+  const [falseValue, setFalseValue] = useState(false);
 
   useEffect(() => {
     async function fetchWords() {
@@ -49,14 +53,18 @@ export default function Words() {
       toast({
         description: 'Correct',
       });
-    }
-    setProgress((prev) => prev + 100 / words.length);
-    setUserInput('');
-    const nextIndex = words.indexOf(currentWord!) + 1;
-    if (nextIndex < words.length) {
-      setCurrentWord(words[nextIndex]);
+
+      setProgress((prev) => prev + 100 / words.length);
+      setUserInput('');
+      setFalseValue(false);
+      const nextIndex = words.indexOf(currentWord!) + 1;
+      if (nextIndex < words.length) {
+        setCurrentWord(words[nextIndex]);
+      } else {
+        setCurrentWord(null);
+      }
     } else {
-      setCurrentWord(null);
+      setFalseValue(true);
     }
   };
 
@@ -74,23 +82,30 @@ export default function Words() {
   }
 
   return (
-    <div className='flex justify-center items-center h-screen'>
-      <div className='text-center'>
-        <h1 className='text-2xl font-bold mb-4'>{currentWord.english}</h1>
-
-        <input
-          type='text'
-          value={userInput}
-          onChange={(e) => setUserInput(e.target.value)}
-          className='border rounded p-2 mb-4 text-sky-400'
-        />
-        <button
-          onClick={checkAnswer}
-          className='bg-blue-500 text-white p-2 rounded'
-        >
-          check
-        </button>
-        <Progress value={progress} className='w-[100%]' />
+    <div className='flex justify-center items-center '>
+      <div className='text-center w-full'>
+        <Card className='m-2'>
+          <CardHeader>
+            <CardTitle>{currentWord.english}</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <Input
+              type='text'
+              value={userInput}
+              onChange={(e) => setUserInput(e.target.value)}
+              className={cn('border rounded p-2 ', {
+                'border-red-500 text-red-500': falseValue,
+                'text-gray-400': !falseValue,
+              })}
+            />
+          </CardContent>
+          <CardFooter className='flex flex-col justify-between'>
+            <Button onClick={checkAnswer} className='m-4'>
+              check
+            </Button>
+            <Progress value={progress} className='w-[100%]' />
+          </CardFooter>
+        </Card>
       </div>
     </div>
   );

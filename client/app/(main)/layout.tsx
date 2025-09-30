e224fdb9e93be1cb7492a8664f2f09ef9f44c87f
Vlad <brovwg@gmail.com>
2025-09-30 16:06:56 +0200

    fix header

--- client/app/(main)/layout.tsx
+++ client/app/(main)/layout.tsx
@@ -1,15 +1,19 @@
-﻿'use client';
+'use client';
 
-import { ReactNode } from 'react';
+import { ReactNode, useState } from 'react';
+import Navbar from '@/app/components/nav-bar';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
-const queryClient = new QueryClient();
-
 export default function MainLayout({ children }: { children: ReactNode }) {
+  const [queryClient] = useState(() => new QueryClient());
+
   return (
     <QueryClientProvider client={queryClient}>
-      {children}
+      <div className='flex min-h-screen flex-col bg-zinc-950 text-zinc-100'>
+        <Navbar />
+        <main className='flex-1'>{children}</main>
+      </div>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
